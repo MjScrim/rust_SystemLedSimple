c306f6df1d2dc87211d3d52f9606f9dd7aa5ac74
Marcos Andrade <109639621+MjScrim@users.noreply.github.com>
2023-03-13 20:20:23 -0300

    Add files via upload

---
+++ app/target/debug/app.d
@@ -0,0 +1,1 @@
+C:\Users\marco\Desktop\rust\app\target\debug\app.exe: C:\Users\marco\Desktop\rust\app\src\main.rs
---
+++ app/target/debug/deps/app.d
@@ -0,0 +1,5 @@
+C:\Users\marco\Desktop\rust\app\target\debug\deps\app.exe: src\main.rs
+
+C:\Users\marco\Desktop\rust\app\target\debug\deps\app.d: src\main.rs
+
+src\main.rs:
